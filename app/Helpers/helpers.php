f2fbba0a5354062385348a32ce7e342c5e59186d
alfath-rizqiy <pattrizqiy@gmail.com>
2025-07-08 09:41:31 +0700

    Menambahkan halpers di folder app dan autoload

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,8 @@
+if (!function_exists('tanggalID')) {
+    function tanggalID($tanggal)
+    {
+        $value = Carbon\Carbon::parse($tanggal);
+        $parse = $value->locale('id');
+        return $parse->translatedFormat('l, d F Y');
+    }
+}
